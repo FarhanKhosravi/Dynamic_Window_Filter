d0e49ea10d4eddfaed981253320e98dc9bfa06b2
Farhan Khosravi <farhankhosravi@gmail.com>
2025-05-11 14:56:17 +0330

    Editing Comments

--- examples/DWFilter_Basic/DWFilter_Basic.ino
+++ examples/DWFilter_Basic/DWFilter_Basic.ino
@@ -26,12 +26,12 @@
 float n = 0;           // Value for heat increment simulation
 
 
-// Make Window Size smaller/bigger if data scatter is less/more (For MAX6675 the optimal WindowSize is ~0.6)
-float WindowSize = 5;
-
 // Make Window Size smaller/bigger if data scatter is less/more
 // (For MAX6675 the optimal WindowSize is ~0.6 | suggested to use a simple moving average before DWFilter for better results)
 float WindowSize = 5;
+
+// Initialize Filter with the given Window Size
+DWFilter myFilter(WindowSize);
 
 
 void setup() {
