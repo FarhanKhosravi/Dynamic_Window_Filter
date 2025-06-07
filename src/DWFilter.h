a9726008f219603243101654c8eda4ef00fddeb4
Farhan Khosravi <farhankhosravi@gmail.com>
2025-06-07 10:36:12 +0330

    Update DWFilter.h
    site update

--- src/DWFilter.h
+++ src/DWFilter.h
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2025 Farhan Khosravi - http://www.farhankhosravi.ir
+Copyright (c) 2025 Farhan Khosravi - https://farhankhosravi.ir
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
